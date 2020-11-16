f6d602fb3ec6c6a856f555e90bdecf367a6b4288
AbuOlen <do.mriyi@gmail.com>
2020-11-16 20:54:36 +0200

    tasks 1,2 - get dog img by breed & error handling

--- get_dogs_breed_axios/routes/index.js
+++ get_dogs_breed_axios/routes/index.js
@@ -4,24 +4,29 @@ const axios = require('axios');
 
 const options = {};
 
+router.get('/', function(req, res) {
+    options.img_src = '';
+    options.img_src_err = 'Choose breed';
+    res.render('index', options);
+});
+
 /* GET breed image. */
 router.get('/:breed', function(req, res) {
   let url  = `https://dog.ceo/api/breed/${req.params.breed}/images/random`;
   axios.get(url)
   .then(function(response) {
        // handle success
-       options.img_src = response.data.message;
-      res.render('index', options);
+    options.img_src = response.data.message;
+    options.img_src_err = '';
+    res.render('index', options);
   })
-  .catch(function (error) {
+  .catch(function (reject) {
       // handle error
-      console.log(error.code);
+    options.img_src_err = reject.response.data.message;
+    options.img_src = '';
+    res.render('index', options);
+    console.log(reject.response.data.message);
     })
-  .then(function () {
-      // always executed
-
-  });
-  
 });
 
 module.exports = router;
