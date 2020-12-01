86b0cc262437cb7145d5db678c684a1ba183efda
AbuOlen <do.mriyi@gmail.com>
2020-12-01 15:11:05 +0200

    with async/await

--- async_await_cats_countries/routes/index.js
+++ async_await_cats_countries/routes/index.js
@@ -8,36 +8,33 @@ let countries = [];
 let cat_url_dict = {};
 
 const url_cats = "https://api.thecatapi.com/v1/breeds";
-const get_cats = new Promise(function (resolve, reject) {
-  request(url_cats, function (e, r, body) {
-    if (e) {
-      reject(e);
-    } else {
-      let cats = JSON.parse(body).map(function (cat) {
-        let c = {};
-        c.name = cat.name;
-        c.country = cat.origin;
-        c.country_code = cat.country_code;
-        c.id = cat.id;
-        return c;
-      });
-      resolve(cats);
-    }
-  });
-});
-
-const get_img_url = (url) => {
-  return new Promise(function (resolve, reject) {
-    request(url, function (e, r, body) {
-      if (e) {
-        reject(e);
-      } else {
-        let urls = JSON.parse(body);
-        cat_url_dict[`${urls[0].breeds[0].id}`] = urls[0].url;
-        resolve(urls[0].url);
-      }
+const get_cats = async () => {
+  return await axios
+    .get(url_cats)
+    .then((r) => {
+    let cats = r.data.map(function (cat) {
+      let c = {};
+      c.name = cat.name;
+      c.country = cat.origin;
+      c.country_code = cat.country_code;
+      c.id = cat.id;
+      return c;
     });
+    return cats;
   });
+};
+
+const get_img_url = async (url) => {
+  return await axios
+    .get(url)
+    .then((r) => {
+      let urls = r.data;
+      cat_url_dict[`${urls[0].breeds[0].id}`] = urls[0].url;
+      return urls[0].url;
+    })
+    .catch((err) => {
+      console.log("axios err", err);
+    });
 };
 
 router.get("/", function (req, res) {
@@ -52,6 +49,7 @@ router.get("/:id", function (req, res) {
   const get_countries = axios
     .get(url)
     .then(function (res1) {
+      // filter countries by region
       countries = res1.data.map(function (country) {
         let c = {};
         c.name = country.name;
@@ -63,50 +61,56 @@ router.get("/:id", function (req, res) {
     .catch((err) => {
       console.log("axios err", err);
     });
-
-  Promise.all([get_countries, get_cats]).then((r) => {
-    options.countries = [];
-    let urls = [];
-    countries.forEach((country) => {
-      const cats = r[1];
-      let c = cats.filter((cat) => {
-        return cat.country_code === country.country_code;
-      });
-
-      if (c.length) {
-        let card = {};
-        card.name = country.name;
-        card.flag = country.flag;
-        card.cats = c;
-
-        card.cats.forEach((cat) => {
-          let url_img_cat = `https://api.thecatapi.com/v1/images/search?breed_ids=${cat.id}`;
-          urls.push(get_img_url(url_img_cat));
+  //wait till countries and cats are loaded
+  Promise.all([get_countries, get_cats()])
+    .then((r) => {
+      options.countries = [];
+      let urls = [];
+      //find cats who originated from selected region
+      countries.forEach((country) => {
+        const cats = r[1];
+        let c = cats.filter((cat) => {
+          return cat.country_code === country.country_code;
         });
 
-        options.countries.push(card);
-      }
-    });
+        if (c.length) {
+          //create country card
+          let card = {};
+          card.name = country.name;
+          card.flag = country.flag;
+          card.cats = c;
 
-    Promise.all(urls).then((r) => {
-      options.countries.forEach((card) => {
-        card.cats.forEach((cat) => {
-          cat.img_url = cat_url_dict[cat.id];
-        });
-        console.table(card.cats);
+          card.cats.forEach((cat) => {
+            // get url for cat
+            let url_img_cat = `https://api.thecatapi.com/v1/images/search?breed_ids=${cat.id}`;
+            urls.push(get_img_url(url_img_cat));
+          });
+
+          options.countries.push(card);
+        }
       });
-      options.loader_disp = "display: none";
-      res.render("index", options);
-      options.loader_disp = "";
+
+      Promise.all(urls)
+        .then((r) => {
+          //set img url to every cat
+          options.countries.forEach((card) => {
+            card.cats.forEach((cat) => {
+              cat.img_url = cat_url_dict[cat.id];
+            });
+            console.table(card.cats);
+          });
+          //display the page
+          options.loader_disp = "display: none";
+          res.render("index", options);
+          options.loader_disp = "";
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
     })
-    .catch(error => { 
-      console.error(error.message)
+    .catch((err) => {
+      console.log("Promise all err", err);
     });
-
-  })
-  .catch((err) => {
-    console.log("Promise all err", err);
-  });
 });
 
 module.exports = router;
