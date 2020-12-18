f6e396c9a1a4d273683f1457f5d8cfdebf9fc899
AbuOlen <do.mriyi@gmail.com>
2020-12-18 10:14:40 +0200

    upload multiple imgs added

--- upload_multiple_imgs/public/js/main.js
+++ upload_multiple_imgs/public/js/main.js
@@ -7,7 +7,10 @@ formEl.addEventListener('submit', (ev) => {
     let params = new FormData(formEl);
     axios.post('/upload', params)
     .then(r => {
-        answEl.innerHTML = r.data;
+        let imgs = r.data.map((str) => {
+            return `<img src="/img/${str}" loading="lazy">`;
+        });
+        answEl.innerHTML = imgs.join('');
         formEl.reset();
     });
 
--- upload_multiple_imgs/routes/index.js
+++ upload_multiple_imgs/routes/index.js
@@ -6,40 +6,38 @@ const path = require("path");
 const fs = require('fs').promises;
 const options = {};
 
-const filename1 = 'temp.jpg';
-const filepath1 = 'uploads/';
 
 let storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, filepath1);
+    destination: function(req, file, cb) {    // uploading directory 
+        cb(null, 'uploads/');
     },
-    filename: function (req, file, cb) {
-        cb(null, filename1);
+    filename: function (req, file, cb) {    //unique name with extension
+      let str = file.originalname;
+      let dotIndex = str.lastIndexOf('.');
+      file.filename = `${str.slice(0,dotIndex)}-${Date.now()}.${str.slice(dotIndex+1)}`;
+      cb(null, file.filename);    //return renamed file
     }
 });
-let upload = multer({storage: storage});
 
 router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-router.get('/img/:img', (req,res) => {
-    console.log('img.req', req.params.img);
-    let path1 = path.join(__dirname, '../uploads/',req.params.img);
-    console.log(path1);
-    res.sendFile(path1)
-})
+router.get('/img/:id', (req,res) => {
+    console.log('img.req', req.params.id);
+    let pathAbs = path.join(__dirname, '../uploads/',req.params.id);
+    console.log("pathAbs", pathAbs);
+    res.sendFile(pathAbs)
+});
 
-router.post ('/upload', upload.single('thefile2'), (req,res) => {
-    let fname = `${req.body.name + '-' + Date.now()}.jpg`;
-    console.log(fname);
-    fs.rename(`${filepath1}${filename1}`, `${filepath1}${fname}`)
-    .then(() => {
-      console.log('renamed')
-      res.send(`<img src="/img/${fname}">`);
-      res.end();
-    });
+let uploadStorage = multer({storage: storage});
+
+router.post ('/upload', uploadStorage.array('multiple_images', 10), (req,res) => {
+  res.send(req.files.map(el => el.filename));
+  res.end();
 
 });
+
+
 
 module.exports = router;
